fba473c4f4804e961834eba44e85891aae0e28e4
Zhuangh7 <zhuanghq2007@foxmail.com>
2019-02-16 15:54:37 +0800

    init

---
+++ ZPM.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using KingAOP.Aspects;
+using RabbitMQ.Client;
+
+namespace ZProgramMoniter {
+    public delegate bool Moniter(MethodExecutionArgs args);
+    public class ZPM {
+        private static Dictionary<string,Moniter> moniters = new Dictionary<string,Moniter>();
+        public static void Print() {
+            Console.WriteLine("testLibrary2");
+        }
+        public static void RegisterMoniter(string name,Moniter m) {
+            moniters.Add(name, m);
+        }
+        public static Moniter getMoniter(string name) {
+            if (moniters.ContainsKey(name)) {
+                return moniters[name];
+            } else {
+                return null;
+            }
+        }
+        public static  void SendNormalLog(string data) {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("ZPM LOG SENT");
+            try {
+                Thread t1 = new Thread(new ParameterizedThreadStart(ZPM.sendMessageToNormal));
+                t1.Start(data);
+            } catch (Exception e) {
+                Console.WriteLine(e);
+            }
+        }
+        public static  void SendWarnningLog(string data) {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("ZPM WARNNING SENT");
+            try {
+                Thread t1 = new Thread(new ParameterizedThreadStart(ZPM.sendMessageToWarnning));
+                t1.Start(data);
+            } catch (Exception e) {
+                Console.WriteLine(e);
+            }
+        }
+        public static void sendMessageToNormal(object data) {
+            var factory = new ConnectionFactory() { HostName = "www.zhuangh7.cn", UserName = "test", Password = "test" };
+            using (var connection = factory.CreateConnection()) {
+                using (var channel = connection.CreateModel()) {
+                    channel.QueueDeclare(queue: "test",
+                                         durable: false,
+                                         exclusive: false,
+                                         autoDelete: false,
+                                         arguments: null);
+
+                    string message = $"{{\"type\":\"ZPM\",\"data\":{{{data}}}}}";
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "",
+                                         routingKey: "test",
+                                         basicProperties: null,
+                                         body: body);
+                    Console.WriteLine(" [x] Sent {0}", message);
+                }
+            }
+        }
+        public static void sendMessageToWarnning(object data) {
+            var factory = new ConnectionFactory() { HostName = "www.zhuangh7.cn", UserName = "test", Password = "test" };
+            using (var connection = factory.CreateConnection()) {
+                using (var channel = connection.CreateModel()) {
+                    channel.QueueDeclare(queue: "warnning",
+                                         durable: false,
+                                         exclusive: false,
+                                         autoDelete: false,
+                                         arguments: null);
+
+                    string message = $"{{\"type\":\"ZPM_W\",\"data\":{{{data}}}}}";
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "",
+                                         routingKey: "warnning",
+                                         basicProperties: null,
+                                         body: body);
+                    Console.WriteLine(" [x] Sent Warnning {0}", message);
+                }
+            }
+        }
+    }
+}
---
+++ ZPMAspect.cs
@@ -0,0 +1,31 @@
+﻿using KingAOP.Aspects;
+using System;
+using System.Threading;
+
+namespace ZProgramMoniter {
+    public class ZPMAspect : OnMethodBoundaryAspect {
+        string name;
+        public ZPMAspect(string fun) {
+            this.name = fun;
+        }
+        public override void OnEntry(MethodExecutionArgs args) {
+            try {
+                ZPM.SendNormalLog($"\"function_name\":\"{this.FunctionName}\",\"client_ip\":\"{args.Arguments[1]}\",\"client_port\":\"{args.Arguments[2]}\"");
+                Moniter moniter = ZProgramMoniter.ZPM.getMoniter(name);
+                if (!moniter(args)) {
+                    ZPM.SendWarnningLog($"\"function_name\":\"{this.FunctionName}\",\"client_ip\":\"{args.Arguments[1]}\",\"client_port\":\"{args.Arguments[2]}\"");
+                }
+            } catch (Exception e) {
+                Console.WriteLine("error happened");
+            }
+        }
+
+        public override void OnSuccess(MethodExecutionArgs args) {
+        }
+
+        public override void OnExit(MethodExecutionArgs args) {
+        }
+
+
+    }
+}
